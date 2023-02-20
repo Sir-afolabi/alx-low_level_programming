9fc90f8bc7f39bc1c577ff7fb3e95f09657ed41d
Emmanuel <emmanuelafolabi456@gmail.com>
2023-02-20 06:34:46 -0800

    Files that are committed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - A program that uses the print function to print an output
- * Return: success 0
+ * main - Entry point
+ * Return: always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+}
