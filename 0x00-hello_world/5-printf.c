87ac89c67b19ef7acda09d7d872b0cce3a3bd3ae
Emmanuel <emmanuelafolabi456@gmail.com>
2023-02-20 03:30:03 -0800

    List of committed files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses the print function to print an output
+ * Return - 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
